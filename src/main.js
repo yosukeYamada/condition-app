c4fbac5dd3e75136a1833a48a60c16fb1bdf638d
yosukeYamada <yamada_141@yahoo.co.jp>
2020-04-22 21:37:36 +0900

    動作検証用のAPIを作成

--- src/main.js
+++ src/main.js
@@ -2,11 +2,29 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
 
+import firebase from 'firebase'
 Vue.config.productionTip = false
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCkTAVQThEpr3S4e_cMkP4xbTSvfDD6rVo",
+  authDomain: "condition-meter.firebaseapp.com",
+  databaseURL: "https://condition-meter.firebaseio.com",
+  projectId: "condition-meter",
+  storageBucket: "condition-meter.appspot.com",
+  messagingSenderId: "118802890827",
+  appId: "1:118802890827:web:f06a0a4a0a62886a0b0dce",
+  measurementId: "G-K0VL0M6KJN"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-text-field v-model="testMessage"></v-text-field>
   </div>
 </template>
 
@@ -13,6 +14,11 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data(){
+    return{
+      testMessage:""
+    }
   }
 }
 </script>

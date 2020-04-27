b1f98487820f09411c2db844e4961a30e90815c7
yosukeYamada <yamada_141@yahoo.co.jp>
2020-04-27 13:31:57 +0900

    localとデプロイ環境で環境変数を更新できるように修正

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  // dev:{
+    // host:'localhost',
+    // port:'8081'
+  // }
+  devServer: {
+    port: 8888,
+    disableHostCheck: true,
+}
 }

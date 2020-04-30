25286c47d2c01eef1e97e8db847e0dbc4aa0a1d5
yuichiyasui <yassan.b.a.w@gmail.com>
2020-05-01 00:15:09 +0900

    今日のコンディション登録の送信パラメーターを変更

--- src/components/daily-post/Posts.vue
+++ src/components/daily-post/Posts.vue
@@ -10,41 +10,43 @@
         <b-form>
           <b-form-group label="今日のモチベーションはどれくらいですか？">
             <b-form-radio-group class="mb-4" v-model="param.motivationSelected">
-              <b-form-radio value="stormy">嵐</b-form-radio>
-              <b-form-radio value="rainy">雨</b-form-radio>
-              <b-form-radio value="cloudy">曇り</b-form-radio>
-              <b-form-radio value="sunny">晴れ</b-form-radio>
-              <b-form-radio value="clear">快晴</b-form-radio>
+              <b-form-radio value="1">快晴</b-form-radio>
+              <b-form-radio value="2">晴れ</b-form-radio>
+              <b-form-radio value="3">曇り</b-form-radio>
+              <b-form-radio value="4">雨</b-form-radio>
+              <b-form-radio value="5">嵐</b-form-radio>
             </b-form-radio-group>
           </b-form-group>
           <b-form-group label="今日の体調はどうですか？">
             <b-form-radio-group class="mb-4" v-model="param.conditionSelected">
-              <b-form-radio value="stormy">嵐</b-form-radio>
-              <b-form-radio value="rainy">雨</b-form-radio>
-              <b-form-radio value="cloudy">曇り</b-form-radio>
-              <b-form-radio value="sunny">晴れ</b-form-radio>
-              <b-form-radio value="clear">快晴</b-form-radio>
+              <b-form-radio value="1">快晴</b-form-radio>
+              <b-form-radio value="2">晴れ</b-form-radio>
+              <b-form-radio value="3">曇り</b-form-radio>
+              <b-form-radio value="4">雨</b-form-radio>
+              <b-form-radio value="5">嵐</b-form-radio>
             </b-form-radio-group>
           </b-form-group>
           <b-form-group label="今日やることの目標のイメージはできていますか？">
-            <b-form-radio-group class="mb-4" v-model="param.performanceSelected">
-              <b-form-radio value="stormy">嵐</b-form-radio>
-              <b-form-radio value="rainy">雨</b-form-radio>
-              <b-form-radio value="cloudy">曇り</b-form-radio>
-              <b-form-radio value="sunny">晴れ</b-form-radio>
-              <b-form-radio value="clear">快晴</b-form-radio>
+            <b-form-radio-group
+              class="mb-4"
+              v-model="param.performanceSelected"
+            >
+              <b-form-radio value="1">快晴</b-form-radio>
+              <b-form-radio value="2">晴れ</b-form-radio>
+              <b-form-radio value="3">曇り</b-form-radio>
+              <b-form-radio value="4">雨</b-form-radio>
+              <b-form-radio value="5">嵐</b-form-radio>
             </b-form-radio-group>
           </b-form-group>
           <b-form-group
             class="mb-4"
-            label="コメント:"
+            label="なにかコメントはありますか？"
             label-for="input-comment"
-            description="何かコメントがあれば入力してください"
           >
             <b-form-textarea
               id="input-comment"
               v-model="param.comment"
-              placeholder="Enter something..."
+              placeholder="コメントがあれば入力してください"
               rows="3"
               max-rows="6"
             ></b-form-textarea>
@@ -68,15 +70,21 @@ export default {
         motivationSelected: "",
         performanceSelected: "",
         conditionSelected: "",
-        comment:""
-        // あとログインユーザーのID情報がいる
+        comment: "",
+        // TODO あとログインユーザーのID情報がいる
       },
     };
   },
   methods: {
     register() {
       axios
-        .get("http://localhost:8080/registerDailyPost")
+        .post("http://localhost:8080/registerDailyPost", {
+          userId: "3", // TODO とりあえず開発用で固定
+          motivationId: this.param.motivationSelected,
+          conditionId: this.param.conditionSelected,
+          performanceId: this.param.performanceSelected,
+          comment: this.param.comment,
+        })
         .then((response) => {
           console.log("コンディション情報の登録に成功しました：" + response);
         })

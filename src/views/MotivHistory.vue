478c5e20c88a35ffe872cae506f3806f16d6f463
IidaShuhei <shuhei.iida@rakus-partners.co.jp>
2020-05-02 17:46:07 +0900

    モチベ履歴デザイン

--- src/views/MotivHistory.vue
+++ src/views/MotivHistory.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-    <Motivations />
-  </div>
+ <b-col>
+    <b-card class="m-5" border-variant="success" 
+      header="モチベーション履歴"
+      header-bg-variant="success"
+      header-text-variant="white"
+    style="border-width:2px;">
+      <Motivations />
+    </b-card>
+ </b-col>
 </template>
 
 <script>

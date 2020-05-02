a8a414dadcc036f8faf4fb1579f523a2ab436af2
yuichiyasui <yassan.b.a.w@gmail.com>
2020-05-03 04:59:52 +0900

    タイトルを少し大きくした

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -2,7 +2,7 @@
   <div>
     <b-navbar type="dark" variant="success">
       <router-link to="/">
-        <b-navbar-brand>Rakuppo</b-navbar-brand>
+        <b-navbar-brand class="ml-1 mr-5 display-1 font-weight-bold">Rakuppo</b-navbar-brand>
       </router-link>
       <v-app-bar-nav-icon class="mr-auto ml-5 text-white">
         <b-button variant="success" v-b-toggle.sidebar-backdrop>

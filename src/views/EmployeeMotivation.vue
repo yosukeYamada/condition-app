779605943e42c96a304735860c539799611e5955
yuichiyasui <yassan.b.a.w@gmail.com>
2020-05-11 12:35:51 +0900

    EmployeeConditionをコンポーネント分割

--- src/views/EmployeeMotivation.vue
+++ src/views/EmployeeMotivation.vue
@@ -1,132 +1,49 @@
 <template>
   <div>
     <h4>
-      {{this.$store.state.employeeList.filter(elm => elm.userId === this.$route.params.userId)[0].hireDate | moment}}入社
-      {{this.$store.state.employeeList.filter(elm => elm.userId === this.$route.params.userId)[0].dep.depName}}
-      {{this.$store.state.employeeList.filter(elm => elm.userId === this.$route.params.userId)[0].userName}}のモチベーション履歴
+      {{
+        this.$store.state.employeeList.filter(
+          (elm) => elm.userId === this.$route.params.userId
+        )[0].hireDate | moment
+      }}入社
+      {{
+        this.$store.state.employeeList.filter(
+          (elm) => elm.userId === this.$route.params.userId
+        )[0].dep.depName
+      }}
+      {{
+        this.$store.state.employeeList.filter(
+          (elm) => elm.userId === this.$route.params.userId
+        )[0].userName
+      }}のコンディション履歴
     </h4>
-    <v-data-table :headers="headers" :items="employeeList">
-      <template v-slot:item.motivation="{ item }">
-        <v-fa
-          :icon="transferIcon(item.motivation)"
-          size="lg"
-          :style="transferColor(item.motivation)"
-        />
-      </template>
-      <template v-slot:item.condition="{ item }">
-        <v-fa
-          :icon="transferIcon(item.condition)"
-          size="lg"
-          :style="transferColor(item.condition)"
-        />
-      </template>
-      <template v-slot:item.performance="{ item }">
-        <v-fa
-          :icon="transferIcon(item.performance)"
-          size="lg"
-          :style="transferColor(item.performance)"
-        />
-      </template>
-    </v-data-table>
+    <EmployeeConditionList />
   </div>
 </template>
 
 <script>
-import moment from 'moment'
+import moment from "moment";
 import { mapActions } from "vuex";
 import axios from "axios";
 import firebase from "firebase/app";
-
+import EmployeeConditionList from "@/components/employee-condition/EmployeeConditionList.vue";
 export default {
-   data() {
-    return {
-      employeeList:[],
-      headers: [
-        {
-          value: "date",
-          text: "投稿日",
-          sortable: true,
-        },
-        {
-          value: "motivation",
-          text: "やる気",
-          sortable: true,
-        },
-        {
-          value: "condition",
-          text: "体調",
-          sortable: true,
-        },
-        {
-          value: "performance",
-          text: "成果",
-          sortable: true,
-        },
-        {
-          value: "comment",
-          text: "コメント",
-          sortable: true,
-        },
-      ],
-    }
-  },
-  mounted() {
-    var list = this.$store.state.employeeList.filter(elm => elm.userId === this.$route.params.userId)
-    for(let num in list[0].dailyPost) {
-      this.employeeList.push({
-        date: moment(list[0].dailyPost[num].date).format("YYYY-MM-DD"), 
-        motivation: list[0].dailyPost[num].postedMotivation.motivation.motivationName,
-        condition: list[0].dailyPost[num].postedCondition.condition.conditionName,
-        performance: list[0].dailyPost[num].postedPerformance.performance.performanceName,
-        comment: list[0].dailyPost[num].postedComment.comment
-      })
-    }
+  components: {
+    EmployeeConditionList,
   },
   filters: {
-    moment: function (date) {
-        return moment(date).format('YYYY年MM月');
-    }
+    moment: function(date) {
+      return moment(date).format("YYYY年MM月");
+    },
   },
   methods: {
-    transferIcon(param) {
-      if (param === "快晴") {
-        return ["fas", "sun"];
-      } else if (param === "晴") {
-        return ["fas", "cloud-sun"];
-      } else if (param === "曇") {
-        return ["fas", "cloud"];
-      } else if (param === "雨") {
-        return ["fas", "cloud-rain"];
-      } else if (param === "嵐") {
-        return ["fas", "cloud-showers-heavy"];
-      } else {
-        return ["fas", "question"];
-      }
-    },
-    transferColor(param) {
-      if (param === "快晴") {
-        return { color: "#ea5550" };
-      } else if (param === "晴") {
-        return { color: "#f3981d" };
-      } else if (param === "曇") {
-        return { color: "#b2cbe4" };
-      } else if (param === "雨") {
-        return { color: "#68a4d9" };
-      } else if (param === "嵐") {
-        return { color: "#0075c2" };
-      } else {
-        return { color: "black" };
-      }
-    },
     //これがないとfirabaseのユーザー情報をstateに格納できない
-     ...mapActions([
-      "setFirebaseUser"
-    ])
+    ...mapActions(["setFirebaseUser"]),
   },
-   //リロード時にログインユーザー情報を保持する
-    init() {
-      firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
-    },
+  //リロード時にログインユーザー情報を保持する
+  init() {
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
+  },
   created() {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
--- src/views/MyCondition.vue
+++ src/views/MyCondition.vue
@@ -1,30 +1,25 @@
 <template>
- <b-col>
+  <b-col>
     <h2 class="mb-4">モチベーション履歴</h2>
-      <Motivations />
- </b-col>
+    <MyConditionList />
+  </b-col>
 </template>
 
 <script>
-import Motivations from "../components/Motivations.vue";
+import MyConditionList from "@/components/my-condition/MyConditionList.vue";
 import axios from "axios";
 import firebase from "firebase/app";
 import { mapActions } from "vuex";
 
-
-
 export default {
   components: {
-    Motivations,
-    
+    MyConditionList,
   },
-  
   //リロード時にログインユーザー情報を保持する
   init() {
     firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
   },
   created() {
-    
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         this.setFirebaseUser(user);
@@ -40,11 +35,9 @@ export default {
       }
     });
   },
-   //これがないとfirabaseのユーザー情報をstateに格納できない
-  methods:{
-    ...mapActions([
-      "setFirebaseUser"
-    ])
-  }
+  //これがないとfirabaseのユーザー情報をstateに格納できない
+  methods: {
+    ...mapActions(["setFirebaseUser"]),
+  },
 };
 </script>

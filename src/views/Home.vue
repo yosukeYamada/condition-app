6a50e53a451db7570b35216f4c24915145744fc8
yuichiyasui <yassan.b.a.w@gmail.com>
2020-05-12 22:34:22 +0900

    homeのコンポーネントを整理

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,22 +2,30 @@
   <b-container>
     <b-row align-v="center" align-h="center">
       <b-col>
-        <AdminHome v-if="authority === 1"></AdminHome>
-        <EmployeeHome v-if="authority === 2"></EmployeeHome>
+        <h2 class="mb-4">ホーム</h2>
+        <b-row>
+          <b-col sm="8">
+            <Infomation />
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col sm="8">
+            <RecentPosts v-if="authority === 1" />
+          </b-col>
+        </b-row>
       </b-col>
     </b-row>
   </b-container>
 </template>
 
 <script>
-import AdminHome from "@/components/home/AdminHome";
-import EmployeeHome from "@/components/home/EmployeeHome";
+import Infomation from "@/components/home/Infomation";
+import RecentPosts from "@/components/home/RecentPosts";
 export default {
   components: {
-    AdminHome,
-    EmployeeHome,
+    Infomation,
+    RecentPosts,
   },
-
   computed: {
     authority: function() {
       return this.$store.state.loginUser.authority;

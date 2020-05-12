2a9fe13c876bd3452bcd29e3a98ce560099737fe
SakaiAkihiro <akihiro.sakai@rakus-partners.co.jp>
2020-05-12 11:44:52 +0900

    お知らせ投稿の途中経過

---
+++ src/components/admin-setting/NewsPost.vue
@@ -0,0 +1,35 @@
+<template>
+  <b-modal id="news-post" centered title="お知らせの投稿">
+    <v-subheader>
+      <b-row>
+        <b-col sm="10" class="pr-0 pb-1">
+          <b-form-input
+          v-model="inputNews"
+            type="textarea"
+            size="sm"
+            placeholder="お知らせを追加"
+            required
+          ></b-form-input>
+        </b-col>
+        <b-col sm="2" class="pb-1">
+          <b-button size="sm" @click="addNews">投稿</b-button>
+        </b-col>
+      </b-row>
+    </v-subheader>
+  </b-modal>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      inputNews:""
+    };
+  },
+  methods:{
+    addNews(){
+      
+    }
+  }
+}
+</script>
---
+++ src/components/admin-setting/NewsPostSetting.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-col sm="6">
+    <b-card
+      border-variant="success"
+      style="border-width:2px;"
+      header="お知らせ投稿"
+      header-bg-variant="success"
+      header-text-variant="white"
+    >
+      <b-card-text>
+        <div class="mb-2">
+          <span
+            v-b-modal.news-post
+            class="blue--text text--darken-3"
+            :style="{ textDecoration: 'none' }"
+            >お知らせの登録を行う</span>
+            <NewsPost />
+        </div>
+      </b-card-text>
+    </b-card>
+  </b-col>
+</template>
+
+<script>
+import NewsPost from "@/components/admin-setting/NewsPost"
+export default {
+  components: {
+    NewsPost
+  },
+  methods: {
+    /** モック用 */
+    toPage() {
+      alert("このページはまだ開発されてません！");
+    },
+  },
+};
+
+</script>

2768c3475f37d5a4c519eb5d28d3c1ec87a069c2
yuichiyasui <yassan.b.a.w@gmail.com>
2020-05-15 04:18:43 +0900

    部署編集機能実装完了

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import Vuex from "vuex";
 import firebase from "firebase/app";
 import "firebase/auth";
 import createPersistedState from "vuex-persistedstate";
+import axios from "axios";
 
 Vue.use(Vuex);
 
@@ -39,8 +40,8 @@ const initialState = {
   firebaseUser: null,
   employeeList: [],
   loginStatus: false,
-  filterDepName:"",
-  newsPost:[],
+  filterDepName: "",
+  newsPost: [],
 };
 
 export default new Vuex.Store({
@@ -74,15 +75,31 @@ export default new Vuex.Store({
       state.depList = depList;
     },
 
-    setFilterDepName(state,filterDepName){
-      state.filterDepName = filterDepName
+    setFilterDepName(state, filterDepName) {
+      state.filterDepName = filterDepName;
     },
-    setData(state,data){
-      state.employeeList = data
+    setData(state, data) {
+      state.employeeList = data;
     },
 
-    setNewsPost(state,newsPost){
+    setNewsPost(state, newsPost) {
       state.newsPost = newsPost;
+    },
+    addNewDep(state, newDep) {
+      state.depList.push(newDep);
+    },
+    changeDepName(state, newDepName) {
+      state.depList.map((dep) => {
+        if (dep.depId === newDepName.depId) {
+          dep.depName = newDepName.depName;
+          dep.updateUserId = newDepName.updateUserId;
+          dep.updateDate = newDepName.updateDate;
+          dep.version = newDepName.version;
+        }
+      });
+    },
+    deleteDep(state, depId) {
+      state.depList = state.depList.filter((dep) => dep.depId !== depId);
     },
   },
   actions: {
@@ -121,19 +138,56 @@ export default new Vuex.Store({
       commit("setDairyPost", dailyPost);
     },
 
-    setFilterDepName({commit},filterDepName){
-      commit("setFilterDepName",filterDepName)
+    setFilterDepName({ commit }, filterDepName) {
+      commit("setFilterDepName", filterDepName);
     },
-    setNewsPost({commit},newsPost){
-      commit("setNewsPost",newsPost)
+    setNewsPost({ commit }, newsPost) {
+      commit("setNewsPost", newsPost);
     },
-    setData({commit},data){
-      commit("setData",data)
-    }
+    setData({ commit }, data) {
+      commit("setData", data);
+    },
+    addNewDep({ commit }, newDepData) {
+      axios
+        .post("/editDeps/addNewDep", newDepData)
+        .then((response) => {
+          alert("部署の新規追加が完了しました！");
+          commit("addNewDep", response.data);
+        })
+        .catch((error) => {
+          alert("部署の新規追加に失敗しました！");
+          console.error(error);
+        });
+    },
+    changeDepName({ commit }, newDepName) {
+      axios
+        .post("/editDeps/changeDepName", newDepName)
+        .then((response) => {
+          alert("部署名の変更が完了しました！");
+          commit("changeDepName", response.data);
+        })
+        .catch((error) => {
+          alert("部署名の変更に失敗しました！");
+          console.error(error);
+        });
+    },
+    deleteDep({ commit }, deletedDep) {
+      axios
+        .post("/editDeps/deleteDep", {
+          depId: deletedDep.depId,
+          updateUserId: deletedDep.updateUserId,
+        })
+        .then(() => {
+          alert(deletedDep.depName + "の削除に成功しました");
+          commit("deleteDep", deletedDep.depId);
+        })
+        .catch((error) => {
+          console.error(error);
+          alert("登録されている部署の削除に失敗しました。");
+        });
+    },
   },
   modules: {
-    
-
     setNewsPost({ commit }, newsPost) {
       commit("setNewsPost", newsPost);
     },
@@ -150,17 +204,15 @@ export default new Vuex.Store({
     },
 
     //従業員リストを部署名と入社年月で絞り込む
-    filterDepName:function(state){
-      let data  = state.employeeList;
-     
+    filterDepName: function(state) {
+      let data = state.employeeList;
+
       //部署名で検索
-      if(state.filterDepName !== ""){
-        data = data.filter(
-          (employeeList) => employeeList.dep.depName
-        );
+      if (state.filterDepName !== "") {
+        data = data.filter((employeeList) => employeeList.dep.depName);
       }
-      return data
-    }
+      return data;
+    },
   },
   plugins: [createPersistedState({ storage: window.sessionStorage })], // オプションを追加
 });

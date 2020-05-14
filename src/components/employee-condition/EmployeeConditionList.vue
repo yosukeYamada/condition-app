50b1a422d5e3614a592e01b733bffd07ebf7df8f
IidaShuhei <shuhei.iida@rakus-partners.co.jp>
2020-05-14 09:15:46 +0900

    不要コード削除

--- src/components/employee-condition/EmployeeConditionList.vue
+++ src/components/employee-condition/EmployeeConditionList.vue
@@ -115,30 +115,6 @@ export default {
         return { color: "black" };
       }
     },
-  //   //これがないとfirabaseのユーザー情報をstateに格納できない
-  //    ...mapActions([
-  //     "setFirebaseUser"
-  //   ])
-  // },
-  //  //リロード時にログインユーザー情報を保持する
-  //   init() {
-  //     firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
-  //   },
-  // created() {
-  //   firebase.auth().onAuthStateChanged((user) => {
-  //     if (user) {
-  //       this.setFirebaseUser(user);
-  //       axios
-  //         .post("/mail/findByMailAndAuthority", {
-  //           mail: firebase.auth().currentUser.email,
-  //         })
-  //         .then((response) => {
-  //           //authorityの値をstateに格納
-  //           this.$store.dispatch("setAuthority", response.data.authority);
-  //           this.$store.dispatch("setLoginUser", response.data);
-  //         });
-  //     }
-  //   });
   },
 };
 </script>

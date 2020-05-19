12ef2acba4a39a4c81ae291207b552c1f4b04da8
yuichiyasui <yassan.b.a.w@gmail.com>
2020-05-20 01:33:54 +0900

    home画面のレイアウトを微調整

--- src/components/home/RecentPosts.vue
+++ src/components/home/RecentPosts.vue
@@ -2,84 +2,78 @@
   <b-card
     border-variant="success"
     style="border-width:2px;"
-    header="本日未投稿者リスト"
+    header="本日の未投稿者リスト"
     header-bg-variant="success"
     header-text-variant="white"
+    no-body
   >
-    <b-card-text>
-      <div class="mb-5" v-for="(latestPost,i ) in latestPosts" :key="i">
-        <p class="mb-2 font-weight-bold">
+    <b-list-group flush id="resent-posts-list">
+      <b-list-group-item v-for="(latestPost, i) in latestPosts" :key="i">
+        <div>
           <span>{{ latestPost.name + "さん" }}</span>
-          <span>   {{ latestPost.post + "です"}}</span>
-        </p>
-      </div>
+          <span> {{ latestPost.post + "です" }}</span>
+        </div>
+      </b-list-group-item>
+    </b-list-group>
+    <b-card-text v-if="latestPosts.length === 0">
+      <p class="grey--text">本日の未投稿者はいません</p>
     </b-card-text>
   </b-card>
-  
 </template>
 <script>
 import moment from "moment";
-import axios from "axios";
 export default {
-  watch: {},
   data() {
     return {
       items: [],
-      latestPosts: []
+      latestPosts: [],
     };
   },
   methods: {
-    setLatestPosts(param){
-      var latestPosts = []
-      var resultPosts = []
+    setLatestPosts(param) {
+      var latestPosts = [];
+      var resultPosts = [];
       var preToday = new Date();
-     
-      for(let i = 0; i < param.length; i++){
+
+      for (let i = 0; i < param.length; i++) {
         var latestPost = {
-        date: "",
-        name: "",
-        post: ""
-        }
+          date: "",
+          name: "",
+          post: "",
+        };
         //投稿が０の場合
-        if(param[i].dailyPost.length === 0){
-          latestPost.date = "2020-05-13T00:43:14.943+0000" //今日以前の日付
-          latestPost.name = param[i].userName
-          latestPost.post = "未投稿"
+        if (param[i].dailyPost.length === 0) {
+          latestPost.date = "2020-05-13T00:43:14.943+0000"; //今日以前の日付
+          latestPost.name = param[i].userName;
+          latestPost.post = "未投稿";
           latestPosts.push(latestPost);
           //投稿がある場合
-        }else{
-          latestPost.date = param[i].dailyPost[0].date
-          latestPost.name = param[i].userName
-          latestPost.post = ""
+        } else {
+          latestPost.date = param[i].dailyPost[0].date;
+          latestPost.name = param[i].userName;
+          latestPost.post = "";
           latestPosts.push(latestPost);
         }
-        
       }
-      for(let i = 0; i < latestPosts.length; i++){
+      for (let i = 0; i < latestPosts.length; i++) {
         // var dateStr = param[i].dailyPost[0].date
-          var str = latestPosts[i].date;
-          var result = str.split('T');
-          var result2 = result[0].split('-');
-          var rdate = new Date(
-            result2[0],
-            result2[1] -1,
-            result2[2],
-            0,
-            0
-          )
-          latestPosts[i].date = rdate;
+        var str = latestPosts[i].date;
+        var result = str.split("T");
+        var result2 = result[0].split("-");
+        var rdate = new Date(result2[0], result2[1] - 1, result2[2], 0, 0);
+        latestPosts[i].date = rdate;
       }
-      
-      for(let i = 0; i < latestPosts.length; i++){
-        if(moment(preToday).isAfter(latestPosts[i].date,"day")){
+
+      for (let i = 0; i < latestPosts.length; i++) {
+        if (moment(preToday).isAfter(latestPosts[i].date, "day")) {
           var resultPost = {
             date: "",
             name: "",
-            post: ""
-          }
-          resultPost.date = latestPosts[i].date
-          resultPost.name = latestPosts[i].name
-          resultPost.post = "未投稿"        
+            post: "",
+          };
+          resultPost.date = latestPosts[i].date;
+          resultPost.name = latestPosts[i].name;
+          resultPost.post = "未投稿";
           resultPosts.push(resultPost);
         }
       }
@@ -87,22 +81,16 @@ export default {
     },
   },
   created() {
-    axios.get("/showEmployeeList")
-    .then((response) => {
-
-      this.$store.dispatch("setEmployeeList", response.data);
-
-      this.items = this.$store.state.employeeList;
-
-      this.latestPosts = this.setLatestPosts(this.items);
-
-    })
+    this.items = this.$store.state.employeeList;
+    this.latestPosts = this.setLatestPosts(this.items);
   },
 };
 </script>
-<style>
-.theme--light.v-data-table thead tr th {
-  background-color: #28a745 !important;
-  color: white !important;
+
+<style scoped>
+#resent-posts-list {
+  max-height: 200px;
+  overflow-y: scroll;
+  overflow-x: hidden;
 }
 </style>

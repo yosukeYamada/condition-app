34657d60f0339c1598f8f128580a77ebd694be78
Higashi <shun053013@gmail.com>
2020-05-19 11:43:11 +0900

    リファクタリング

--- src/store/index.js
+++ src/store/index.js
@@ -67,7 +67,7 @@ export default new Vuex.Store({
     setLoginUser(state, user) {
       state.loginUser = user;
     },
-     /**
+    /**
      * firebaseの情報をstateにセットする
      * @param {*} user ユーザー
      */
@@ -322,8 +322,8 @@ export default new Vuex.Store({
           alert("登録されている部署の削除に失敗しました。");
         });
     },
-    deleteUser({commit},employee){
-      commit("deleteUser",employee.userId);
+    deleteUser({ commit }, employee) {
+      commit("deleteUser", employee.userId);
     },
     /**
      * お知らせ投稿一覧を取得するメソッド
@@ -331,8 +331,8 @@ export default new Vuex.Store({
      */
     getNewsList({ commit }) {
       axios.get("/showNewsList").then((response) => {
-          commit("setNewsPost", response.data);
-        })
+        commit("setNewsPost", response.data);
+      });
     },
   },
   getters: {
@@ -343,19 +343,10 @@ export default new Vuex.Store({
     /**
      * ログインユーザーのアイコンをfirebaseから取得する
      */
-    photoURL: (state) => state.firebaseUser ? state.firebaseUser.photoURL : "",
+    photoURL: (state) =>
+      state.firebaseUser ? state.firebaseUser.photoURL : "",
     getStatus: function(state) {
       return state.loginStatus;
-    },
-    //従業員リストを部署名と入社年月で絞り込む
-    filterDepName: function(state) {
-      let data = state.employeeList;
-
-      //部署名で検索
-      if (state.filterDepName !== "") {
-        data = data.filter((employeeList) => employeeList.dep.depName);
-      }
-      return data;
     },
   },
   plugins: [createPersistedState({ storage: window.sessionStorage })], // オプションを追加

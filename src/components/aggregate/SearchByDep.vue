14f86684b07baac797e5bbbe25e7387c9c4f4635
IidaShuhei <shuhei.iida@rakus-partners.co.jp>
2020-05-20 18:02:39 +0900

    最新取り込み

---
+++ src/components/aggregate/SearchByDep.vue
@@ -0,0 +1,68 @@
+<template>
+  <el-select id="dep" v-model="filterDepName" clearable placeholder="部署名">
+    <el-option
+      v-for="depName in this.$store.state.depList"
+      :key="depName.depId"
+      :label="depName.depName"
+      :value="depName.depId"
+    >
+    </el-option>
+  </el-select>
+</template>
+<script>
+import { mapGetters } from "vuex";
+export default {
+  data() {
+    return {
+      depNameArray: [],
+
+      inputDepName: "",
+
+      filteredAggregateList: {},
+    };
+  },
+  computed: {
+    ...mapGetters(["filterAggregateList"]),
+    /** storeのdepListの取得 */
+    setDepNameArray: function() {
+      return this.$store.state.depList;
+    },
+    filterDepName: {
+      get() {
+        return this.inputDepName;
+      },
+      set(value) {
+        this.inputDepName = value;
+      },
+    },
+  },
+  watch: {
+    /** storeのdepListの変更の監視 */
+    setDepNameArray: function(newDepList) {
+      let depNameArray = newDepList.map((dep) => dep.depName);
+      depNameArray.push("全ての従業員");
+      this.depNameArray = depNameArray;
+    },
+    //カテゴリー検索（部署名）の変更の監視
+    filterDepName: function() {
+      this.$store.dispatch("setFilterDepName", this.inputDepName);
+    },
+  },
+
+  beforeDestroy() {
+    this.$store.dispatch("setFilterDepName", "");
+  },
+
+  mounted() {
+    let depNameArray = this.$store.state.depList.map((dep) => dep.depName);
+    depNameArray.push("全ての従業員");
+    this.depNameArray = depNameArray;
+  },
+};
+</script>
+<style>
+#dep:hover {
+  cursor: pointer;
+  border-color: #60eeff;
+}
+</style>

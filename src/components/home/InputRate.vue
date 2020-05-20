4ac4de437132f86534a6ae7dd8eb9e1e2c2c32a8
SakaiAkihiro <akihiro.sakai@rakus-partners.co.jp>
2020-05-20 14:33:55 +0900

    入力値のグラフ作成

--- src/components/home/InputRate.vue
+++ src/components/home/InputRate.vue
@@ -1,19 +1,16 @@
 <template>
-  <b-card
+<b-card
     border-variant="success"
     style="border-width:2px;"
     header="本日の入力率"
     header-bg-variant="success"
     header-text-variant="white"
+    no-body
   >
-    <InputPieChart></InputPieChart>
-
-    <!-- <b-card-text>
-          <div>{{ "未投稿：" + latestPosts.length + "人/" + items.length + "人"}}</div>
-          <div>{{ items.length - latestPosts.length }}</div>
-    </b-card-text> -->
-
-  </b-card>
+  <div class="small">
+    <InputPieChart :chart-data="inputChartData" :options="options" :is-get-data="isGetData"></InputPieChart>
+  </div>
+</b-card>
 </template>
 
 <script>
@@ -21,17 +18,47 @@ import InputPieChart from "@/components/home/InputPieChart";
 import moment from "moment";
 
 export default {
-  watch: {},
   components: {
     InputPieChart
   },
+  props: ["selectedDate"],
   data() {
     return {
-      items: [],
-      latestPosts: []
+      totalNnumbers:[],
+      postedNnumbers: [],
+      unpostedNnumbers: [],
+      isGetData: false,
+      inputChartData: null,
+      options: {
+        responsive: true,
+        legend: {
+          display: false
+        }
+      }
     };
   },
   methods: {
+    fillData() {
+      this.inputChartData = {
+        labels: [this.getPosted(), this.getUnPosted()],
+        datasets: [
+          {
+            label: "Data",
+            backgroundColor: "#f87979",
+            data: [this.getPosted(), this.getUnPosted()]
+          }
+        ]
+      };
+    },
+    getPosted() {
+      this.totalNnumbers = this.$store.state.employeeList;
+      return this.totalNnumbers.length - this.unpostedNnumbers.length;
+      // return 1;
+    },
+    getUnPosted() {
+      return this.unpostedNnumbers.length;
+      // return 3;
+    },
     setLatestPosts(param) {
       var latestPosts = [];
       var resultPosts = [];
@@ -40,14 +67,20 @@ export default {
       for (let i = 0; i < param.length; i++) {
         var latestPost = {
           date: "",
+          name: "",
+          post: "",
         };
-        //投稿が０の場合
+        //投稿が0の場合
         if (param[i].dailyPost.length === 0) {
           latestPost.date = "2020-05-13T00:43:14.943+0000"; //今日以前の日付
+          latestPost.name = param[i].userName;
+          latestPost.post = "未投稿";
           latestPosts.push(latestPost);
           //投稿がある場合
         } else {
           latestPost.date = param[i].dailyPost[0].date;
+          latestPost.name = param[i].userName;
+          latestPost.post = "";
           latestPosts.push(latestPost);
         }
       }
@@ -63,17 +96,36 @@ export default {
         if (moment(preToday).isAfter(latestPosts[i].date, "day")) {
           var resultPost = {
             date: "",
+            name: "",
+            post: "",
           };
           resultPost.date = latestPosts[i].date;
+          resultPost.name = latestPosts[i].name;
+          resultPost.post = "未投稿";
           resultPosts.push(resultPost);
         }
       }
       return resultPosts;
-    }
+    },
+  },
+  watch: {
+    selectedDate: {
+      handler: function() {
+        this.fillData();
+      },
+    },
   },
   created() {
-    this.items = this.$store.state.employeeList
-    this.latestPosts = this.setLatestPosts(this.items)
-  }
+    this.totalNnumbers = this.$store.state.employeeList;
+    this.unpostedNnumbers = this.setLatestPosts(this.totalNnumbers);
+    this.fillData();
+  },
 };
 </script>
+
+<style>
+.small {
+  max-height: 200px;
+  max-width: 200px;
+}
+</style>

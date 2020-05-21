b938e51ab5a7f13ad373bba6c73527db91546e61
yuichiyasui <yassan.b.a.w@gmail.com>
2020-05-21 21:11:02 +0900

    トップページの画像が見切れてたのを修正

--- src/components/top/About.vue
+++ src/components/top/About.vue
@@ -8,12 +8,16 @@
           <div class="title font-weight-bold mb-4">
             コンディションを可視化する
           </div>
-          <div
-            class="mx-auto d-flex align-items-center mb-5 card elevation-3 "
-            style="width:360px;"
-          >
-            <img src="/img/graph.gif" style="width:360px;" />
-          </div>
+          <b-row align-h="center">
+            <b-col lg="5" md="6" sm="8" cols="11">
+              <img
+                id="gif-img"
+                style="object-fit: cover;width:100%;"
+                src="/img/graph.gif"
+                class="mx-auto img-fluid elevation-3 mb-5"
+              />
+            </b-col>
+          </b-row>
           <p class="mx-auto">
             Rakuppoはリクルートとサイバーエージェントにより共同開発されたアプリケーション「Geppo」に強く感銘を受け、
             未経験エンジニアによって開発された社内向けコンディション可視化アプリケーションです。
@@ -27,12 +31,15 @@
           <div class="title font-weight-bold mb-4">
             自分のコンディションを振り返る
           </div>
-          <div
-            class="mx-auto d-flex align-items-center mb-4 card elevation-3"
-            style="width:360px;"
-          >
-            <img src="/img/my_condition.png" style="width:360px;" />
-          </div>
+          <b-row align-h="center">
+            <b-col lg="5" md="6" sm="8" cols="11">
+              <img
+                src="/img/my_condition.png"
+                class="mx-auto img-fluid elevation-3 mb-5"
+                style="object-fit: contain;"
+              />
+            </b-col>
+          </b-row>
           <p class="mx-auto mb-0">
             今日の自分のコンディションを天気で表した5段階で自己採点します。
           </p>
@@ -58,12 +65,14 @@
           <div class="title font-weight-bold mb-4">
             社内のコンディションを俯瞰する
           </div>
-          <div
-            class="mx-auto d-flex align-items-center mb-4 card elevation-3"
-            style="width:360px;"
-          >
-            <img src="/img/employee_list.png" style="width:360px;" />
-          </div>
+          <b-row align-h="center">
+            <b-col lg="5" md="6" sm="8" cols="11">
+              <img
+                src="/img/employee_list.png"
+                class="mx-auto img-fluid elevation-3 mb-5"
+              />
+            </b-col>
+          </b-row>
           <p class="mx-auto">
             管理者ユーザーは、従業員が登録したコンディションを閲覧することができます。
           </p>
@@ -76,33 +85,6 @@
     </b-container>
   </b-col>
 </template>
-<script>
-export default {
-  data() {
-    return {
-      items: [
-        {
-          title: "Rakkupoとは",
-          descriptions: [
-            "Rakuppoは、働く従業員が自らのコンディションを登録し管理するシステムです。",
-            "コンディションについて、このアプリケーションでは3つの観点から定義しました。",
-            "(1)モチベーション ",
-            "(2)体調 ",
-            "(3)目標へのイメージ ",
-          ],
-        },
-        {
-          title: "開発環境",
-          descriptions: [
-            "開発言語：Java(SpringBoot), JavaScript(Vue.js)",
-            "実行環境：Firebase, Heroku, Heroku Postgres",
-          ],
-        },
-      ],
-    };
-  },
-};
-</script>
 <style scoped>
 hr {
   background: linear-gradient(to right, green, yellow);

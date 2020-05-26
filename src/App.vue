57feb9ba8df1f6c68df466ebdfb576be76c6370d
Higashi <shun053013@gmail.com>
2020-05-26 10:46:06 +0900

    更新

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,6 @@
 <script>
 import Header from "@/components/common/Header";
 import SideMenu from "@/components/common/SideMenu";
-import firebase from "firebase";
-import axios from "axios";
-import { mapActions } from "vuex";
 
 export default {
   name: "App",
@@ -19,32 +16,8 @@ export default {
     Header,
     SideMenu,
   },
-  mounted() {
-    // axios.get('test/insert')
-    this.loginCheck();
-  },
-  methods: {
-    ...mapActions(["setLoginUser"]),
-    loginCheck() {
-      firebase.auth().onAuthStateChanged((user) => {
-        if (!user) {
-          /** ログインしていない場合 */
-          console.log("ログインしていません");
-        } else {
-          /** ログインしている場合 */
-          console.log("ログインしています");
-          if (this.$store.state.loginUser === null) {
-            axios
-              .post("/loginCheck", {
-                mail: firebase.auth().currentUser.email,
-              })
-              .then((response) => {
-                this.setLoginUser(response.data);
-              });
-          }
-        }
-      });
-    },
-  },
+  
+
+  
 };
 </script>

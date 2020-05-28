7cf011d07db7e3e506ee074c701aacd39fe20992
IidaShuhei <shuhei.iida@rakus-partners.co.jp>
2020-05-29 07:53:22 +0900

    ラクスカル君追加

--- src/components/home/ChatBox.vue
+++ src/components/home/ChatBox.vue
@@ -1,5 +1,16 @@
 <template>
-  <section class="chat-box">
+  <v-container>
+    <div class="rakuscal">
+      <v-img 
+        :src="rakuscalImage"
+        class="img"
+        @click="open()"
+        v-model="toggle"
+      />
+      <div class="description" v-if="!toggle">僕とおしゃべりしたい人は<br>僕をクリックしてね。</div>
+      <div class="description" v-if="toggle">僕とおしゃべりを終えたい人は僕をクリックしてね。</div>
+    </div>
+  <section class="chat-box" v-if="toggle">
     <div class="chat-box-list-container" ref="chatbox">
       <ul class="chat-box-list">
         <li
@@ -22,15 +33,18 @@
       <button @click="sendMessage">送信</button>
     </div>
   </section>
+  </v-container>
 </template>
 
 <script>
-import axios from "axios";
+import axios from 'axios'
 export default {
   name: 'ChatBox',
   data: () => ({
     message: '',
-    messages: []
+    messages: [],
+    toggle:false,
+    rakuscalImage: "/rakuscal_clear.png",
   }),
   methods: {
     sendMessage() {
@@ -46,22 +60,22 @@ export default {
       axios.post('https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk', params)
       .then(res => {
         this.messages.push({
-          text: "ラクスカル : "+res.data.results[0].reply,
+          text: res.data.results[0].reply,
           author: 'server'
         })
         this.$nextTick(() => {
           this.$refs.chatbox.scrollTop = this.$refs.chatbox.scrollHeight
         })
       })
+    },
+    open() {
+      this.toggle = !this.toggle
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
-.chat-box {
-  background: white;
-}
 .chat-box,
 .chat-box-list {
   display: flex;
@@ -71,48 +85,46 @@ export default {
 .chat-box-list-container {
   overflow: scroll;
   margin-bottom: 1px;
+  margin-top: 5px;
 }
 .chat-box-list {
   padding-left: 10px;
   padding-right: 10px;
   span {
-    padding: 8px;
+    padding: 5px;
     color: white;
-    border-radius: 4px;
+    border-radius: 10px;
   }
   .server {
     span {
       background: #99cc00;
     }
     p {
-      float: right;
+      float: left;
     }
   }
   .client {
     span {
+      height: 20px;
       background: #0070C8;
     }
     p {
-      float: left;
+      float: right;
     }
   }
 }
 .chat-box {
-  margin: 10px;
   border: 1px solid #999;
-  width: 30vw;
-  height: 50vh;
+  width: 50vw;
+  height: 40vh;
   border-radius: 4px;
-  margin-left: auto;
-  margin-right: auto;
-  align-items: space-between;
   justify-content: space-between;
 }
 .chat-inputs {
   display: flex;
   
   input {
-    line-height: 3;
+    line-height: 2;
     width: 100%;
     border: 1px solid #999;
     border-left: none;
@@ -130,5 +142,50 @@ export default {
     border-right:none;
     border-bottom-right-radius: 3px;
   }
+}
+.img {
+  width: 200px;
+  height: 200px;
+}
+.img:hover {
+  cursor: pointer;
+}
+.rakuscal{
+    position: relative;
+    cursor: pointer;
+    display: inline-block;
+}
+.rakuscal p{
+    margin:0;
+    padding:0;
+}
+.description {
+    display: none;
+    position: absolute;
+    padding: 10px;
+    font-size: 12px;
+    line-height: 1.6em;
+    color: #fff;
+    border-radius: 5px;
+    background: #000;
+    width: 190px;
+    font-weight: bold;
+}
+.description:before {
+    content: "";
+    position: absolute;
+    top: -24px;
+    right: 60%;
+    border: 15px solid transparent;
+    border-top: 15px solid #000;
+    margin-left: -15px;
+    transform: rotateZ(180deg);
+}
+.rakuscal:hover .description{
+    display: inline-block;
+    left: 20px;
+}
+.container {
+  display: flex;
 }
 </style>

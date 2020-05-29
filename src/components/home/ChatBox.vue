82fd7295d4849f78ba6e5a7d65960e5670473f18
IidaShuhei <shuhei.iida@rakus-partners.co.jp>
2020-05-29 13:04:37 +0900

    ラクスカル君にアクション追加

--- src/components/home/ChatBox.vue
+++ src/components/home/ChatBox.vue
@@ -4,13 +4,40 @@
       <v-img 
         :src="rakuscalImage"
         class="img"
-        @click="open()"
-        v-model="toggle"
       />
-      <div class="description" v-if="!toggle">僕とおしゃべりしたい人は<br>僕をクリックしてね。</div>
-      <div class="description" v-if="toggle">僕とおしゃべりを終えたい人は僕をクリックしてね。</div>
+
+      <div class="description" v-if="!toggle">
+        僕に何か用かな？
+        <ul>
+          <li @click="openTalk" id="talk">ラクスカル君とおしゃべりしたい！</li>
+          <li @click="openLastWeek" id="lastWeek">私の先週の働きっぷりを総評して欲しい！</li>
+          <li @click="openLastMonth" id="lastMonth">私の先月の働きっぷりを総評して欲しい！</li>
+        </ul>
+      </div>
+
+      <div class="finish" v-if="talk" @click="closeTalk">
+        <span class="finishTalk">終了するならクリック！</span>
+      </div>
+
+      <div class="finish" v-if="lastWeek" @click="closeLastWeek">
+        <span class="finishTalk">終了するならクリック！</span>
+      </div>
+
+      <div class="finish" v-if="lastMonth" @click="closeLastMonth">
+        <span class="finishTalk">終了するならクリック！</span>
+      </div>
+
     </div>
-  <section class="chat-box" v-if="toggle">
+
+    <div v-if="lastWeek" class="my-parts">
+      <span>こんにちは</span>
+    </div>
+
+    <div v-if="lastMonth" class="my-parts">
+      <span>こんばんは</span>
+    </div>
+
+  <section class="chat-box" v-if="talk">
     <div class="chat-box-list-container" ref="chatbox">
       <ul class="chat-box-list">
         <li
@@ -44,6 +71,9 @@ export default {
     message: '',
     messages: [],
     toggle:false,
+    talk:false,
+    lastWeek: false,
+    lastMonth:false,
     rakuscalImage: "/rakuscal_clear.png",
   }),
   methods: {
@@ -68,9 +98,30 @@ export default {
         })
       })
     },
-    open() {
+    openTalk() {
       this.toggle = !this.toggle
-    }
+      this.talk = !this.talk
+    },
+    closeTalk() {
+      this.toggle = !this.toggle
+      this.talk = !this.talk
+    },
+    openLastWeek() {
+      this.toggle = !this.toggle
+      this.lastWeek = !this.lastWeek
+    },
+    closeLastWeek() {
+      this.toggle = !this.toggle
+      this.lastWeek = !this.lastWeek
+    },
+    openLastMonth() {
+      this.toggle = !this.toggle
+      this.lastMonth = !this.lastMonth
+    },
+    closeLastMonth() {
+      this.toggle = !this.toggle
+      this.lastMonth = !this.lastMonth
+    },
   }
 }
 </script>
@@ -163,12 +214,12 @@ export default {
     display: none;
     position: absolute;
     padding: 10px;
-    font-size: 12px;
+    font-size: 14px;
     line-height: 1.6em;
     color: #fff;
     border-radius: 5px;
-    background: #000;
-    width: 190px;
+    background: green;
+    width: 330px;
     font-weight: bold;
 }
 .description:before {
@@ -177,15 +228,81 @@ export default {
     top: -24px;
     right: 60%;
     border: 15px solid transparent;
-    border-top: 15px solid #000;
+    border-top: 15px solid green;
     margin-left: -15px;
     transform: rotateZ(180deg);
 }
 .rakuscal:hover .description{
     display: inline-block;
-    left: 20px;
+    left: -50px;
 }
 .container {
   display: flex;
+}
+#talk:hover {
+  font-size: 15px;
+}
+#lastWeek:hover {
+  font-size: 15px;
+}
+#lastMonth:hover {
+  font-size: 15px;
+}
+.finish {
+    position: absolute;
+    padding: 10px;
+    font-size: 14px;
+    line-height: 1.6em;
+    color: #fff;
+    border-radius: 5px;
+    background: green;
+    width: 200px;
+    font-weight: bold;
+}
+.finish:before {
+    content: "";
+    position: absolute;
+    top: -24px;
+    right: 60%;
+    border: 15px solid transparent;
+    border-top: 15px solid green;
+    margin-left: -15px;
+    transform: rotateZ(180deg);
+}
+.finish:hover .finishTalk {
+  font-size: 15px;
+}
+.my-parts {
+	display: inline-block;
+	width: 500px;
+	max-width: 100%;
+	background: white;
+	border: 2px solid green;
+	border-radius: 0px;
+	padding: .8em;
+	position: relative;
+	text-align: left;
+}
+.my-parts > :last-child {
+	margin-bottom: 0;
+}
+.my-parts::before,
+.my-parts::after {
+	content: "";
+	position: absolute;
+	top: 50%;
+	right: 100%;
+}
+.my-parts::before {
+	margin-top: -12.82px;
+	border: 12.82px solid transparent;
+	border-right: 12.82px solid green;
+	z-index: 1;
+}
+.my-parts::after {
+	margin-top: -10px;
+	border: 10px solid transparent;
+	border-right: 10px solid green;
+	z-index: 2;
 }
 </style>
